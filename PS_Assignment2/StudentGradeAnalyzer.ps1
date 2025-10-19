25d095b17bf658f6d4e5f8ccf705edf54fdffba4
Bitxz <jstrick2014@outlook.com>
2025-10-19 18:15:16 -0230

    Add files via upload

---
+++ PS_Assignment2/StudentGradeAnalyzer.ps1
@@ -0,0 +1,10 @@
+# Name: Jonathan Strickland
+# Stundent Number: 20165947
+
+# Prompt the user to enter number of grades
+$numOfGrades = Read-Host "Enter Number of grades"
+$grades = @()
+
+foreach ($grade in $grades) {
+
+}
